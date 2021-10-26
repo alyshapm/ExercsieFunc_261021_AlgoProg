6c2faa0b22255a9cbb95cd9a5e03acfac4f405d4
alyshapm <91533472+alyshapm@users.noreply.github.com>
2021-10-26 22:23:48 +0700

    Update Alysha_Ex6.4.py

--- Alysha_Ex6.4.py
+++ Alysha_Ex6.4.py
@@ -2,6 +2,8 @@ def calcNewHeight():
     currentWidth = eval(input("Enter the current width of the image: ")) # input prompt
     currentHeight = eval(input("Enter the current height of the image: "))
     newWidth = eval(input("Enter desired width: "))
+    while newWidth > currentWidth:
+        newWidth = eval(input("Re-enter desired width, must be smaller than current width: ")) # new width must be smaller than current width
     newHeight = newWidth * currentHeight / currentWidth # formula to find the new height, by using the concept of proportions/ratio (cross-multiplication) and / is used to give a float value
     return newHeight 
 
